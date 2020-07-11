3d8b7f45f8eb770d9439f5e914d226127cbc7cb2
Leo Tumwattana <leo.tumwattana@gmail.com>
2020-07-11 08:04:26 +0800

    Fix warnings

--- Sources/HeroPlugin.swift
+++ Sources/HeroPlugin.swift
@@ -154,7 +154,7 @@ extension HeroPlugin {
 // MARK: Plugin Support
 internal extension HeroTransition {
   static func isEnabled(plugin: HeroPlugin.Type) -> Bool {
-    return enabledPlugins.index(where: { return $0 == plugin}) != nil
+    return enabledPlugins.firstIndex(where: { return $0 == plugin}) != nil
   }
 
   static func enable(plugin: HeroPlugin.Type) {
@@ -163,7 +163,7 @@ internal extension HeroTransition {
   }
 
   static func disable(plugin: HeroPlugin.Type) {
-    if let index = enabledPlugins.index(where: { return $0 == plugin}) {
+    if let index = enabledPlugins.firstIndex(where: { return $0 == plugin}) {
       enabledPlugins.remove(at: index)
     }
   }
